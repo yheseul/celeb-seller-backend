8cf5385a1a70590c2fea819050bc4eb7e7300c8f
yheseul <kangyheseul@naver.com>
2025-03-07 21:15:00 +0900

    feat: apply myBatis to member feature

--- src/main/java/com/shop/shop/controller/MemberController.java
+++ src/main/java/com/shop/shop/controller/MemberController.java
@@ -23,18 +23,20 @@ public class MemberController {
 	
 	@PostMapping("login")
 	public Map<String, String> login(@RequestBody Member m, HttpServletRequest request) {
+		System.out.println(m);
 		Map<String,String> responseData=new HashMap();
 		try {
 			m=memberService.login(m);
-			if(m!=null) {//login ok
+			if(m!=null) {
 				HttpSession session=request.getSession();
 				System.out.println(session.getId());
 				session.setAttribute("member", m);
-				responseData.put("msg","ok");
-			}else {//login fail
+				responseData.put("msg","로그인 성공");
+			}else {
+				// 뭔가.. 그 아디나 비번 틀렸다는 정확한 피드백주기...ㅇㅅㅇ(or 없는 계정);
 				responseData.put("msg","다시 로그인 해주세요");
 			}
-		} catch (Exception e) {//login error			
+		} catch (Exception e) {			
 			e.printStackTrace();
 			responseData.put("msg","다시 로그인 해주세요");
 		}
@@ -46,7 +48,7 @@ public class MemberController {
 		Map<String,String> responseData=new HashMap();
 		try {
 			memberService.insertMember(m);
-			responseData.put("msg","ok");
+			responseData.put("msg","회원가입 성공");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
